aafda04ad8e5daffb0c52780d73cbdb2ebe8b18c
aviralsingh108 <admiralaviral@gmail.com>
2022-05-24 00:23:25 +0530

    closures with examples

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
-// CLosures
+// -----------------------Closures----------------
 
+/*
+ Def -- In Javascript, when we return a function from another function, we are effectively returning a combination of the function along with it's function scope. This would let the function definition have an associated persistent memory which could hold on to live data between executions. The combination of the function and its scope chain is what is called as a closure in Javascript.
+ */
+
+/*
+function outer(){
+    counter = 0;
+    function inner(){
+        counter++;
+        console.log(counter)
+    }
+    inner()
+}
+outer();    // OP - 1
+outer();    // OP - 1
+*/
+
+/*
+function outer(){
+    counter = 0;
+    function inner(){
+        counter++;
+        console.log(counter)
+    }
+    return inner;
+}
+fn = outer();    
+fn()    // OP - 1
+fn()    // OP - 2
+*/
---
+++ readme.md
@@ -0,0 +1,3 @@
+## This Repo covers Advanced Javascript Concepts with notes and examples
+
+## To run the file, type node main.js in the terminal
